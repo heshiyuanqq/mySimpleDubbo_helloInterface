b1b7f891325ec279dac5454cf44a4dd310c1aeaa
heshiyuan <15801636814@163.com>
2016-10-25 10:33:09 +0800

    添加person.java

---
+++ src/main/java/demo/demo1/Person.java
@@ -0,0 +1,25 @@
+package demo.demo1;
+
+public class Person {
+		private String userName;
+		private String password;
+		public String getUserName() {
+			return userName;
+		}
+		public void setUserName(String userName) {
+			this.userName = userName;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		@Override
+		public String toString() {
+			return "Person [userName=" + userName + ", password=" + password
+					+ "]";
+		}
+		
+		
+}
